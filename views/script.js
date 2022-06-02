b3c80abce604ba124cc3bb76986074f2569fd03a
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2022-06-02 12:45:52 -0300

    front

--- views/script.js
+++ views/script.js
@@ -1,35 +1,58 @@
-const btn = document.querySelector(".btn-search-movie");
-const movies = document.querySelector(".movies");
-const text = document.querySelector("#movie");
+const btnSearch = document.querySelector(".btn-search-movie");
+const movieNameInput = document.querySelector("#movie-name");
+const moviesDiv = document.querySelector(".movies");
+const errorMessage = document.querySelector(".error-message");
 
-btn.addEventListener("click", (e) =>{
-    e.preventDefault()
-    movies.innerHTML = "";
+const API_URL = 'http://localhost:5000';
 
-    let textVal = text.value
+let movies = [];
+let filteredMovies = [];
 
-    fetch(`http://www.omdbapi.com/?s=${textVal}&apikey=a9b91b45`)
-    .then((response) =>{
-        return response.json();
-    })
-    .then((data) =>{
-        console.log(data)
-        data.Search.forEach(movie => {
-            console.log(movie)
-
-            const box = document.createElement('div');
-            box.setAttribute('class', 'box');
-            box.setAttribute('data-id', movie.imdbID);
-            box.innerHTML = `
-            <img src="${movie.Poster}" alt="">
-            <div class="box-divider">
-              <p><span>${movie.Title}</span> <span>(${movie.Year})</span></p>
-              <p>${movie.Type}</p>
-            </div>`
-            movies.appendChild(box);
-        })
-    })
-    .catch((erro)=>{
-        console.log(erro)
-    })
+window.addEventListener('load', () => {
+    loadMovies()
 })
+
+function searchMovie(e) {
+    e.preventDefault();
+    let searchValue = e.target.value.trim().toLowerCase();
+    if (!!searchValue && searchValue.length > 0) {
+        filteredMovies = movies.filter(movie => movie.titulo.toLowerCase().includes(searchValue))
+        clearMovies();
+        displayMovies(filteredMovies);
+    } else {
+        clearMovies();
+        displayMovies(movies);
+    }
+}
+
+async function loadMovies() {
+    try {
+        let response = await fetch(`${API_URL}/filmes`)
+        movies = await response.json()
+        console.log(movies)
+        displayMovies(movies)
+    } catch (error) {
+        errorMessage.classList.toggle('error-message');
+    }
+}
+
+function displayMovies(movies) {
+    movies.forEach(movie => {
+        const box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        box.dataset.id = movie.filme_id
+        box.innerHTML = `
+            <img src="${movie.imagem}" alt="Foto do filme: ${movie.titulo}">
+            <div class="box-divider">
+              <p><span>${movie.titulo}</span> <span>Likes ${movie.likes}</span></p>
+              <p>${movie.descricao}</p>
+            </div>`
+        moviesDiv.appendChild(box);
+    })
+}
+
+function clearMovies() {
+    while(moviesDiv.firstChild) {
+        moviesDiv.firstChild.remove()
+    }
+}
